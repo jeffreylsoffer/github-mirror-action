bef65f1d17131b2f19dda1cde4ea904669095ada
Jeffrey Soffer <65000982+jeffreylsoffer@users.noreply.github.com>
2022-04-23 20:33:53 -0400

    Update action.sh
    maybe checkout fix?

--- action.sh
+++ action.sh
@@ -6,7 +6,7 @@ if [ -z "${REMOTE}" ]; then
   echo Please specify an origin
   exit 1
 fi
-
+git config --global --add safe.directory /github/workspace
 git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
 git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
